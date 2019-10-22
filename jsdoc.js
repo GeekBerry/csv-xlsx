9b230e3bca0879e5f1e4ef8f55b0166cf470b1b6
Wanghaizhou <geekberry@qq.com>
2019-10-22 16:40:52 +0800

    UPDATE: jsdoc accept with class doc.

--- jsdoc.js
+++ jsdoc.js
@@ -9,12 +9,10 @@ function jsdocToMd(inputPath, outputPath) {
   const files = fs.statSync(inputPath).isDirectory() ? `${inputPath}/**/*.js` : inputPath;
 
   const jsdocData = jsdocApi.explainSync({ files });
-  const fullDataArray = jsdocParse(jsdocData);
-  // console.log(JSON.stringify(fullDataArray, null, 2)); // for DEBUG
+  const fullDataArray = parseJsDoc(jsdocData);
 
-  const filenameToArray = lodash.groupBy(fullDataArray, data => getFilename(data));
-  const headToArray = lodash.mapKeys(filenameToArray, (_, filename) => relativeHead(inputPath, filename));
-  const markdownString = lodash.map(headToArray, (array, head) => dumpFile(head, array)).join('');
+  const ownerToArray = lodash.groupBy(fullDataArray, 'memberof');
+  const markdownString = lodash.map(ownerToArray, (array, owner) => dumpFile(array, owner)).join('');
 
   if (outputPath) {
     fs.writeFileSync(outputPath, markdownString);
@@ -23,51 +21,79 @@ function jsdocToMd(inputPath, outputPath) {
   return markdownString;
 }
 
-function dumpFile(head, array) {
-  // array.forEach(({ kind, name, meta }) => console.log(kind, name, meta.filename)); // for DEBUG
+function parseJsDoc(jsdocData) {
+  const memberArray = [];
+  const globalArray = [];
+
+  jsdocParse(jsdocData).forEach(data => {
+    switch (data.kind) {
+      case 'class':
+        data.kind = 'module';
+        data.memberof = data.name;
+        break;
+
+      case 'constructor':
+        data.kind = 'function';
+        data.memberof = data.name;
+        data.name = 'constructor';
+        break;
+
+      default:
+        break;
+    }
+
+    if (data.memberof) {
+      memberArray.push(data);
+    } else {
+      data.memberof = path.parse(data.meta.filename).name;
+      globalArray.push(data);
+    }
+  });
+  return [...memberArray, ...globalArray];
+}
+
+function dumpFile(array, head) {
+  // array.forEach(({ kind, name, meta }) => console.log(kind, name, (meta || {}).filename)); // for DEBUG
 
   return `
 ----------
 # ${head}
 
-${array.filter(({ kind }) => kind === 'module').map(dumpModule).join('\n\n')}
-${array.filter(({ kind }) => kind === 'member').map(data => dumpMember(head, data)).join('\n')}
-${array.filter(({ kind }) => kind === 'function').map(data => dumpFunction(head, data)).join('\n')}`;
+${array.filter(({ kind }) => kind === 'module').map(dumpModule).join('\n')}
+${array.filter(({ kind }) => kind === 'member').map(dumpMember).join('\n')}
+${array.filter(({ kind }) => kind === 'function').map(dumpFunction).join('\n')}`;
+}
+
+function dumpHead({ memberof, name }) {
+  return memberof ? `${memberof}.${name}` : name;
 }
 
 function dumpModule({ description }) {
   return formatText(description);
 }
 
-function dumpMember(head, data) {
+function dumpMember(data) {
   return `
-## ${head}.${data.name}
+## ${dumpHead(data)}
 
 ${dumpDescription(data)}
 \`${typeName(data.type)}\`
 ${dumpExamples(data)}`;
 }
 
-function dumpFunction(head, data) {
+function dumpFunction(data) {
   // if (data.name === 'constructor') console.log(JSON.stringify(data, null, 2)); // for DEBUG
   return `
-## ${head}.${data.name}
+## ${dumpHead(data)}
 
 ${dumpDescription(data)}
-${dumpSignature(data)}
 ${dumpParams(data)}
 ${dumpReturns(data)}
 ${dumpExamples(data)}`;
 }
 
 function dumpDescription({ description }) {
-  return description ? formatText(description) + '\n' : '';
-}
-
-function dumpSignature({ async, name, params = [] }) {
-  const names = params.map(p => p.name.split('.')[0]);
-
-  return `\`${async ? 'async ' : ''}function ${name}(${lodash.uniq(names).join(',')})\``;
+  return formatText(description);
 }
 
 function dumpParams({ params = [] }) {
@@ -97,7 +123,7 @@ ${str ? str : '`void`'}`;
 }
 
 function dumpExamples({ examples = [] }) {
-  const str = examples.map(e => `${'```js'}\n ${e.trim()}\n${'```'}`).join('\n\n');
+  const str = examples.map(e => `${'```'}\n${e.trim()}\n${'```'}`).join('\n\n');
 
   if (!str) {
     return '';
@@ -120,21 +146,6 @@ function paramName(param) {
 
 function typeName({ names = [] } = {}) {
   return names.join('|');
-}
-
-function getFilename({ meta = {} }) {
-  return path.join(meta.path || '', meta.filename || '');
-}
-
-function relativeHead(inputPath, filename) {
-  const { dir, name } = path.parse(filename);
-  filename = path.join(dir, name);
-
-  return path
-    .relative(inputPath, filename)
-    .split(path.sep)
-    .filter(v => !v.startsWith('.') && v !== 'index')
-    .join('.');
 }
 
 module.exports = {
